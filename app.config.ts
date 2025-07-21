51a76855717212cab2ba03a3f1f5cefbc6c86c08
Jeff Shelton <jeff@shelton.one>
2025-07-20 22:04:06 -0400

    headache

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwind from "@tailwindcss/vite";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import mdx from "@mdx-js/rollup";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  extensions: ["md", "mdx"],
   vite: {
     resolve: {
       alias: {
@@ -14,6 +16,13 @@ export default defineConfig({
         "@/routes": resolve(__dirname, "./src/routes"),
       },
     },
-    plugins: [tailwind()],
+    plugins: [
+      mdx({
+        jsx: true,
+        jsxImportSource: "solid-jsx",
+        providerImportSource: "solid-mdx",
+      }),
+      tailwind(),
+    ],
   },
 });
--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,10 +3,10 @@ import { HttpStatusCode } from "@solidjs/start";
 
 export default function NotFound() {
   return (
-    <main>
+    <main class="flex justify-center items-center w-screen h-screen">
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1 class="text-white font-mono font-bold text-3xl">Page Not Found (404)</h1>
     </main>
   );
 }
